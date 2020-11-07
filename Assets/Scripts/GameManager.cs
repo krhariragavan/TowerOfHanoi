98997a3df60e1fdadf4a6a0d73c0728b6c70070d
krhariragavan <krhariragavan@gmail.com>
2020-11-07 22:05:37 +0530

    Victory Condition Complete
    Coded victory condition of the game.
    Basic gameplay complete.
    Pending work, Losing condition and timer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Run-Time Ref --------->
+    // Clicked Object
+    public GameObject ClickedObject;
+    // Run-Time Ref --------->
+
+    // Scene Ref -------->
+    public GameObject OriginTower;
+    public GameObject VictoryTower1;
+    public GameObject VictoryTower2;
+    // Scene Ref -------->
+
+    [Header ("Set Value")]
+    // Variables ---------->
+    public int TotalCount;
+    // Variables ---------->
+
+    //[HideInInspector]
+    public bool IsWrongMove;
+
+    // Events -------->
+    public delegate void VictoryAchieved_Delegate (); // Event triggered when player successfully achieves the goal
+    public event VictoryAchieved_Delegate OnVictoryAchieved;
+    public void VictoryAchieved () // This function is called in Tower.cs 
+    {
+        OnVictoryAchieved?.Invoke ();
+    }
+
+    public delegate void LostGame_Delegate (); // Losses game when there are no more moves or if there is a time constrain in future
+    public event LostGame_Delegate OnLostGame;
+    // Events -------->
+
+    public static GameManager Instance;
+
+    private void Awake ()
+    {
+        Instance = this;    
+    }
+
+    void Start ()
+    {
+
+    }
+
+    void Update ()
+    {
+        SetClickedObj ();
+    }
+
+    /// <summary>
+    /// Setting up on click values
+    /// </summary>
+    void SetClickedObj ()
+    {
+        if (Input.GetMouseButton (0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast (ray, out raycastHit, 100f))
+            {
+                Interactable interact = raycastHit.collider.GetComponent<Interactable> ();
+                if (interact != null)
+                {
+                    if (interact.CanMove) // Returns true only if it is a top piece
+                    {
+                        ClickedObject = interact.gameObject;
+                    }
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public bool IsVictoryTower; // Except orgin tower other two towers are marked as vicotry tower in inspector
+    public List<GameObject> TowerContains = new List<GameObject> (); // List of all objects that is placed in the tower
+
+    void Start ()
+    {
+
+    }
+
+    void Update ()
+    {
+        if (IsVictoryTower)
+        {
+            if (!GameManager.Instance.IsWrongMove)
+            {
+                if (TowerContains.Count == GameManager.Instance.TotalCount)
+                {
+                    GameManager.Instance.VictoryAchieved ();
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter (Collider other)
+    {
+        Interactable interact = other.GetComponent<Interactable> ();
+        if (interact != null)
+        {
+            TowerContains.Add (other.gameObject);
+            Debug.Log ("Enter --->" + other.gameObject.name);
+        }
+
+    }
+
+    private void OnTriggerExit (Collider other)
+    {
+        Interactable interact = other.GetComponent<Interactable> ();
+        if (interact != null)
+        {
+            TowerContains.Remove (other.gameObject);
+            Debug.Log ("Exit --->" + other.gameObject.name);
+        }
+    }
+
+    private void OnTriggerStay (Collider other)
+    {
+        //Debug.Log ("Stay --->" + other.gameObject.name);
+    }
+}
